4a7eef10a0873fefb95ad8aeba3899bfdfaacf76
Sofia <burovasofia@list.ru>
2022-10-21 18:29:39 +0300

    урок 3

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h4>Проверка работа</h4>")
 
-# Create your views here.
+def about(request):
+    return HttpResponse("<h4>Про нас</h4>")
