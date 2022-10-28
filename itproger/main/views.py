02640942d2f844d623515ca12b56190687ec32ed
Sofia <burovasofia@list.ru>
2022-10-28 11:48:15 +0300

    урок 6

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
+    data = {
+        'title': 'Главная страница',
+        'values': ['some', 'hello', '123'],
+        'obj': {
+            'car': "BMW",
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
